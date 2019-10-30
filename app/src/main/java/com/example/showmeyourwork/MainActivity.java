4fdbb572829478a4f1b6c42ad5ebc984c9a46c11
TEO <ted243@naver.com>
2019-10-30 14:36:03 +0900

    webview layout, class 완성

--- app/src/main/java/com/example/showmeyourwork/MainActivity.java
+++ app/src/main/java/com/example/showmeyourwork/MainActivity.java
@@ -2,18 +2,40 @@ package com.example.showmeyourwork;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button videoVidsBtn;
+    Button webViewBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        videoVidsBtn = findViewById(R.id.activity_mainactivity_video_btn);
+        videoVidsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ActivityVideoPlay.class);
+                startActivity(intent);
+            }
+        });
 
-        Log.e("테스트", "테스트");
-        Log.e("테스트", "테스트");
+        webViewBtn = findViewById(R.id.webview_btn);
+        webViewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ActivityWebViewPlay.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
 }
