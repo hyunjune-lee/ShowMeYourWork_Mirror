29b7895313ebaae2f82c6ed9d23b52e0cca3cab5
TEO <ted243@naver.com>
2019-11-02 20:05:24 +0900

    webview로 javascript 실행시킨 후 다시 crawling 하는 방법 구현중..

--- app/src/main/java/lunchgym/first/showmeyourwork/MainActivity.java
+++ app/src/main/java/lunchgym/first/showmeyourwork/MainActivity.java
@@ -1,38 +1,22 @@
 package lunchgym.first.showmeyourwork;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.os.Build;
-import android.util.Log;
-import android.view.SurfaceView;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.Button;
-
-import org.opencv.android.BaseLoaderCallback;
-import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Mat;
-
-import java.util.Collections;
-import java.util.List;
-
-import static android.Manifest.permission.CAMERA;
 
 
 public class MainActivity extends AppCompatActivity{
     Button cameraBtn;
     Button exoplayerBtn;
     Button webviewBtn;
+    Button crawlingBtn;
     Intent intent;
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +24,7 @@ public class MainActivity extends AppCompatActivity{
         cameraBtn = findViewById(R.id.camera_btn);
         exoplayerBtn = findViewById(R.id.exoplayer_btn);
         webviewBtn = findViewById(R.id.webview_btn);
+        crawlingBtn = findViewById(R.id.crawling_btn);
 
         cameraBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,6 +45,14 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onClick(View v) {
                 intent = new Intent(MainActivity.this, ExoplayerActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        crawlingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(MainActivity.this, CrawlingActivity.class);
                 startActivity(intent);
             }
         });
